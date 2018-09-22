ce88032961b2d2a9b4a441f41c75c78021bc36a4
Matthew Campbell <cattmampbell@gmail.com>
2018-09-22 16:55:49 -0400

    Refactored scripts.js file.

--- scripts.js
+++ scripts.js
@@ -1,125 +1,85 @@
-// Listens for the page to load, then runs a function().
-document.addEventListener("DOMContentLoaded", function() {
-    let div = document.createElement("div"); // Creates <div> tag.
-    div.className = "header-container"; // Gives a class name / applies styling via Bootstrap 4.
-    document.body.appendChild(div); // Places <div class="header=container"> into <body> of HTML.
-    
-    let divRow1 = document.createElement("div"); // Creates <div class="row">.
-    divRow1.className = "row";
-    div.appendChild(divRow1);
+// 'DOMContentLoaded' event handler on document
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body');
 
-    let divCol1 = document.createElement("div"); // Creates <div class="col-12">.
-    divCol1.className = "col-12 text-center";
-    divRow1.appendChild(divCol1);
+    // Creates <elementName class="className"> and appends the element via appendTo
+    const createElement = (elementName, className, appendTo) => {
+        const element = document.createElement(elementName);
+        element.className = className;
+        appendTo.appendChild(element);
+        return element;
+    }
 
-    let btn = document.createElement("button"); // Creates <button>.
-    let btnTxt = document.createTextNode("Add Square"); // Creates textual content.
-    btn.appendChild(btnTxt); // Adds textual content to <button>.
-    btn.className = "btn col-4 btn-lg btn-outline-secondary px-2 py-2 mx-2 my-4";
-    divCol1.appendChild(btn);
+    const divContainer = createElement('div', 'header-container', body);
+    const divRow1 = createElement('div', 'row', divContainer);
+    const divCol1 = createElement('div', 'col-12 text-center', divRow1);
 
-    let divRow2 = document.createElement("div"); // Creates <div class="row">.
-    divRow2.className = "row";
-    div.appendChild(divRow2);
+    const btn = createElement('button', 'btn col-4 btn-lg btn-outline-secondary px-2 py-2 mx-2 my-4', divCol1);
+    const btnTxt = document.createTextNode('Add Square'); 
+    btn.appendChild(btnTxt); 
 
-    let divCol2 = document.createElement("div"); // Creates <div class="col-12">.
-    divCol2.className = "col-12";
-    divRow2.appendChild(divCol2);
+    const divRow2 = createElement('div', 'row', divContainer);
+    const divCol2 = createElement('div', 'col-12', divRow2);
 
-    let idCount = 0; // Creates 'idCount', set value to 0.
+    let idCount = 0; // Initializes idCount (number of squares on the page)
+   
+    // Creates <div class="div-square" id="idCount"> and appends it to divCol2, then creates <h1>idCount</h1> and appends it to divSquare
+    const addSquare = () => {
+        const divSquare = createElement('div', 'div-square', divCol2); 
+        idCount += 1; // Adds 1 to idCount each time addSquare() is called
+        divSquare.id = idCount; 
 
-    // Creates addSquare() function.
-    function addSquare() {
-        let divSquare = document.createElement("div"); // Creates <div class="div-square">.
-        divSquare.className = "div-square";
-        divCol2.appendChild(divSquare);
-        idCount++; // Adds +1 to 'idCount' with each iteration of addSquare() function.
-        divSquare.id = idCount;
-
-        let divHead = document.createElement("h1"); // Creates <h1>.
-        let divTxt = document.createTextNode(idCount);
+        const divHead = createElement('h1', 'h1 text-center text-light justify-content-center align-items-center mt-4', divSquare);
+        divHead.style.visibility = 'hidden';
+        const divTxt = document.createTextNode(idCount);
         divHead.appendChild(divTxt);
-        divHead.className = "h1 text-center text-light justify-content-center align-items-center mt-4";
-        divHead.style.visibility = "hidden"; // Sets the CSS 'visible' Property to a value of 'hidden'. 
-        divSquare.appendChild(divHead);
 
-        // Creates showTxt() function.
-        function showTxt() {
-            divHead.style.visibility = "visible"; // Changes the CSS 'visible' Property to a value of 'visible'. 
-        };
+        const showTxt = () => { divHead.style.visibility = 'visible'; } // Sets visibility: visible;
+        divSquare.addEventListener('mouseover', showTxt); // 'mouseover' event handler on divSquare, calls showTxt()
+        
+        const hideTxt = () => { divHead.style.visibility = 'hidden'; } // Sets visibility: hidden;
+        divSquare.addEventListener('mouseout', hideTxt); // 'mouseout' event handler on divSquare, calls hideTxt()
 
-        // Listens for <div class="div-square"> to be moused over, then runs showTxt() function.
-        divSquare.addEventListener("mouseover", showTxt);
+        const myColors = [ '#007bff', '#6c757d', '#28a745', '#dc3545', '#ffc107', '#17a2b8', '#f8f9fa', '#343a40' ];
 
-        // Creates hideTxt() function.
-        function hideTxt() {
-            divHead.style.visibility = "hidden";
-        };
-
-        // Listens for <div class="div-square"> to be moused out, then runs hideTxt() function.
-        divSquare.addEventListener("mouseout", hideTxt);
-
-        // Creates an array of myColors[].
-        let myColors = [
-            "#007bff", // Equal to "primary" (blue).
-            "#6c757d", // Equal to "secondary" (dark grey).
-            "#28a745", // Equal to "success" (green).
-            "#dc3545", // Equal to "danger" (red).
-            "#ffc107", // Equal to "warning" (yellow).
-            "#17a2b8", // Equal to "info" (teal).
-            "#f8f9fa", // Equal to "light" (light grey).
-            "#343a40" // Equal to "dark" (black).
-        ];
-
-        // Creates getRandomColor() function.
-        function getRandomColor() {
-            let myRandomColor = myColors[Math.floor(Math.random()*myColors.length)]; // Randomly selects an item from myColors[].
+        // Randomly selects an index from myColors, assigns value to myRandomColor
+        const getRandomColor = () => {
+            const myRandomColor = myColors[ Math.floor (Math.random() * myColors.length) ];
             return myRandomColor;
-        };
+        }
 
-        // Listens for <div class="div-square"> to be clicked, then runs a function().
-        divSquare.addEventListener("click", function() {
-            let myRandomColor = getRandomColor();
-            divSquare.style.backgroundColor = myRandomColor;
-        });
+        // 'click' event handler on divSquare, calls getRandomColor() and sets return as value of background-color:
+        divSquare.addEventListener('click', () => {
+            const thisRandomColor = getRandomColor();
+            divSquare.style.backgroundColor = thisRandomColor;
+        })
 
-        // Creates ifEven() function.
-        function ifEven() {
-            alert("This is even!");
-            /*
-            if() {
-               
-            } else if() {
-                alert("There aren't any squares after the clicked square!");
-            };
-            */
-        };
-
-        // Creates ifOdd() function.
-        function ifOdd() {
-            alert("This is odd!");
-            /*
-            if() {
-                
-            } else if() {
-                alert("There aren't any squares before the clicked square!");
-            };
-            */
-        };
-
-        // Listens for <div class="div-square"> to be double clicked, then runs a function().
-        divSquare.addEventListener("dblclick", function() {
-            // console.log(this.id); 
-            if(this.id % 2 == 0) {
-                ifEven();
-                // alert("This is even!");
-            } else {
-                ifOdd();
-                // alert("This is odd!");
-            };
-        });
+        // 'dblclick' event handler on divSquare
+        divSquare.addEventListener('dblclick', () => { 
+            const divSquareId = parseInt(divSquare.id);
+            const firstDivSquare = divCol2.firstElementChild;
+            const firstDivSquareId = parseInt(firstDivSquare.id);
+            const lastDivSquare = divCol2.lastElementChild;
+            const lastDivSquareId = parseInt(lastDivSquare.id);
+            const divSquareTotal = document.querySelectorAll('div-square').length;
+            if(divSquareId === 1 && divSquareId === firstDivSquareId && divSquareId === lastDivSquareId && divSquareId === divSquareTotal) {
+                alert(`There are no more squares, this is the only one! Click button to add more!`); 
+            } else if(divSquareId === 1 && divSquareId < divSquareTotal) {
+                alert(`Click the button to add more squares to the page!`);
+            } else if(divSquareId / 2 !== 0) { // Odd
+                const deletePreviousDivSquare = divSquare.previousElementSibling;
+                const deletePreviousDivSquareId = deletePreviousDivSquare.id;
+                alert(`Well, isn't this odd?`);
+            } else if(divSquareId > 1 && divSquareId !== firstDivSquareId && divSquareId !== lastDivSquareId && divSquareId > deletePreviousDivSquareId) {
+                alert(`Goodbye, ${deletePreviousDivSquareId}!`);
+            } else if(divSquareId % 2 == 0) { Even
+                alert(`This is even, I'd say!`);
+                const deleteNextDivSquare = divSquare.nextElementSibling;
+                const deleteNextDivSquareId = deleteNextDivSquare.id;
+            }
+        })
     };
 
-    // Listens for <button> to be clicked, then runs addSquare() function.
-    btn.addEventListener("click", addSquare);
-}); 
+    // 'click' event handler on <button>
+    btn.addEventListener('click', addSquare); 
+})
